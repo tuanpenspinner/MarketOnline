18aac3f9a3386621ef1fa500fd28db72f70083df
tuanpenspinner <tuanpenspinnner>
2021-03-18 13:28:47 +0700

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-success">f</button>
+      <i className="fas fa-plus"></i>
     </div>
   );
 }
