b090359d6449be5c5e234a4e4efc785510c38622
tuanpenspinner <tuanpenspinnner>
2021-03-24 22:03:00 +0700

    Change route

--- src/configs/routers.config.js
+++ src/configs/routers.config.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import routes from "../routers";
 import RouterCustom from "./route";
 
